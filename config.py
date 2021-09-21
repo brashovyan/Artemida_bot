3fba46a578fec53f22f16dd09d3cda23cc26f64a
Brashovyan Artem <artembrash@mail.ru>
2021-09-21 23:37:39 +0500

    ver 1.03

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': '',
+    'token': 'ODg5NDEwODI3NTI0OTI3NTE5.YUg2YQ.lMIo9NUW6H0l-aqWo-vTE-iPpK8',
     'bot': 'Artemida bot',
     'id': 889410827524927519,
     'prefix': '$'
