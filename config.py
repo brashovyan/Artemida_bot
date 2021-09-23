1c2708860e7453c59c13fc71476f8c674570c1a3
Brashovyan Artem <artembrash@mail.ru>
2021-09-23 19:03:08 +0500

    ver 1.061

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODg5NDEwODI3NTI0OTI3NTE5.YUg2YQ.HWZokdCIl_SoXyZn2aNYFvmIK4E',
+    'token': '',
     'bot': 'Artemida bot',
     'id': 889410827524927519,
     'prefix': '$'
