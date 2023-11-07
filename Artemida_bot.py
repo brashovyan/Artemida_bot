58a42e2d1b9f9c524f7d6ae61fbebd0e2c5bdcf9
Brashovyan Artem <artembrash@mail.ru>
2023-11-07 17:57:36 +0500

    work 2023

--- Artemida_bot.py
+++ Artemida_bot.py
@@ -1,147 +1,42 @@
 from discord.ext import commands
 from config import settings
-import json
 import requests
 import discord
 import random
-from googletrans import Translator
-
-#from discord.utils import get
-
 from asyncio import sleep
-from youtube_dl import YoutubeDL
-
 import os
 from bs4 import BeautifulSoup
-from aiobalaboba import balaboba
-from aiohttp import ClientSession
-
-
+import yt_dlp
 
 #перед запуском не забудь вставить токен
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'False'}
 bot = commands.Bot(command_prefix=settings['prefix'], intents = intents, help_command=None)  # Так как мы указали префикс в settings.
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+ytdl_options = {
+    "format": "bestaudio/best",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "noplaylist": False,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": (
+        "0.0.0.0"
+    ),  # Bind to ipv4 since ipv6 addresses cause issues at certain times
+}
 
-@bot.command()
-async def p(ctx): #случайная пикча с интернета (с определенного сайта)
-    r = random.randint(1, 14)
-    if r == 1:
-        response = requests.get('https://some-random-api.ml/img/pikachu')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Anime')
-        embed.set_image(url=json_data['link'])
-        await ctx.send(embed=embed)
-
-    elif r == 2:
-        response = requests.get('https://some-random-api.ml/animu/wink')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Anime')
-        embed.set_image(url=json_data['link'])
-        await ctx.send(embed=embed)
-    elif r == 3:
-        response = requests.get('https://some-random-api.ml/animu/pat')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Anime')
-        embed.set_image(url=json_data['link'])
-        await ctx.send(embed=embed)
-    elif r == 4:
-        response = requests.get('https://some-random-api.ml/meme')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Meme')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
-    elif r == 5:
-        response = requests.get('http://aws.random.cat/meow')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Cat')
-        embed.set_image(url=json_data['file'])
-        await ctx.send(embed=embed)
-    elif r == 6:
-        response = requests.get('https://some-random-api.ml/animal/panda')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Panda')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
-    elif r == 7:
-        response = requests.get('https://some-random-api.ml/animal/dog')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Dog')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
-    elif r == 8:
-        response = requests.get('https://some-random-api.ml/animal/cat')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Cat')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
-    elif r == 9:
-        response = requests.get('https://some-random-api.ml/animal/fox')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Fox')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
-    elif r == 10:
-        response = requests.get('https://some-random-api.ml/animal/red_panda')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Red Panda')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
-    elif r == 11:
-        response = requests.get('https://some-random-api.ml/animal/koala')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Koala')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
-    elif r == 12:
-        response = requests.get('https://some-random-api.ml/animal/birb')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Bird')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
-    elif r == 13:
-        response = requests.get('https://some-random-api.ml/animal/raccoon')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title='Random Racoon')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
-    elif r == 14:
-        response = requests.get('https://some-random-api.ml/animal/kangaroo')
-        json_data = json.loads(response.text)  # Извлекаем JSON
-
-        embed = discord.Embed(color=0xff9900, title='Random Kangaroo')
-        embed.set_image(url=json_data['image'])
-        await ctx.send(embed=embed)
 
 @bot.command() #случайное число от 0 до 100
 async def roll(message):
     r = random.randint(0, 100)
     await message.send(f"{message.author.mention }: {str(r)}")
 
-@bot.command() #цитата из аниме
-async def s(ctx):
-    response = requests.get('https://some-random-api.ml/animu/quote')
-    json_data = json.loads(response.text)  # Извлекаем JSON
-    translator = Translator()
-    result = translator.translate(json_data['sentence'], src='en', dest='russian')
-    s = "Цитата: " + json_data['sentence']+"\n" + "\nАниме: " + json_data['anime'] + "\nПерсонаж: " + json_data['character'] +'\n' +'\n' +"Перевод: " + result.text
-    await ctx.send(s)
 
 @bot.command() #музыка с ютуба. $play ссылка
 async def play(ctx, arg):
@@ -155,24 +50,25 @@ async def play(ctx, arg):
             if vc.is_playing():
                 await ctx.send(f'{ctx.message.author.mention}, Видео добавлено в очередь.')
             else:
-                await ctx.send(f'{ctx.message.author.mention} В данный момент вы не находитесь в голосовом канале.')
+                await ctx.send(f'{ctx.message.author.mention} Ошибка! Возможно, вы не находитесь в голосовом канале. Либо попробуйте через минуту.')
         except:
-            await ctx.send(f'{ctx.message.author.mention} В данный момент вы не находитесь в голосовом канале.')
+            await ctx.send(f'{ctx.message.author.mention} Ошибка! Возможно, вы не находитесь в голосовом канале. Либо попробуйте через минуту.')
 
 
     while vc.is_playing():
         await sleep(1)
 
-    with YoutubeDL(YDL_OPTIONS) as ydl:
+    with yt_dlp.YoutubeDL(ytdl_options) as ydl:
         info = ydl.extract_info(arg, download=False)
-    URL = info['formats'][0]['url']
-    vc.play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=URL, **FFMPEG_OPTIONS))
+        URL = info['url']
+        vc.play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=URL, **FFMPEG_OPTIONS))
+
 
 @bot.command() #локальная музыка с компа
 async def playl(ctx):
     global vc
     i = 0
-    path = "C:\\Games\\Muzlo" #путь до папки с музыкой. умеет скипать то, что не может воспроизвести
+    path = "D:\\Muzlo" #путь до папки с музыкой. умеет скипать то, что не может воспроизвести
     filelist = []
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -187,11 +83,11 @@ async def playl(ctx):
     except:
         try:
             if vc.is_playing():
-                await ctx.send(f'{ctx.message.author.mention}, Вы уже запустили локальную музыку. Если вы хотите переключить музыку, воспользуйтесь командой $skip. Если вы хотите включить видео с Ютуба, сперва используйте команду $stop, потом $play + ссылка. Если музыка не воспроизваодится, повторите попытку через минуту.')
+                await ctx.send(f'{ctx.message.author.mention}, Ошибка! Бот уже что-то проигрывает, скорее всего ютуб. Сначала выключите его, написав $leave или $stop.')
             else:
-                await ctx.send(f'{ctx.message.author.mention} В данный момент вы не находитесь в голосовом канале.')
+                await ctx.send(f'{ctx.message.author.mention} Ошибка! Возможно, вы не находитесь в голосовом канале.')
         except:
-            await ctx.send(f'{ctx.message.author.mention} В данный момент вы не находитесь в голосовом канале.')
+            await ctx.send(f'{ctx.message.author.mention} Ошибка! Возможно, вы не находитесь в голосовом канале.')
 
     if vc.is_playing() == False:
         while True:
@@ -210,23 +106,29 @@ async def playl(ctx):
                 n = n[:-4]
                 await ctx.send(f'Сейчас проигрывается: {n}')
 
-@bot.command()
-async def anekdot(ctx):
-    page = requests.get(f"https://vse-shutochki.ru/anekdoty/{random.randint(1, 2059)}")
-    page1 = page.content
-    soup = BeautifulSoup(page1, "lxml")
-    soup1 = soup.find_all("div", {"class":"post"})
-    #print(soup1)
-    str = soup1[random.randint(0, len(soup1)-1)].text.split("\n")
-    await ctx.send(str[0])
+
+# короче дискорд без танцев с бубном теперь на даёт отправлять запросы в инет(
+# python requests и сайт тут не причем
+# @bot.command()
+# async def anekdot(ctx):
+#     page = requests.get(f"https://vse-shutochki.ru/anekdoty/{random.randint(1, 2059)}")
+#     page1 = page.content
+#     soup = BeautifulSoup(page1, "lxml")
+#     soup1 = soup.find_all("div", {"class":"post"})
+#     #print(soup1)
+#     str = soup1[random.randint(0, len(soup1)-1)].text.split("\n")
+#     await ctx.send(str[0])
+
 
 @bot.command()
 async def stop(ctx):
     await vc.disconnect()
 
+
 @bot.command()
 async def leave(ctx):
     await vc.disconnect()
+
 
 @bot.command()
 async def skip(ctx):
@@ -236,33 +138,28 @@ async def skip(ctx):
     except:
         vc.stop()
 
-@bot.command()
-async def bal(ctx):
-    str = ctx.message.content.split("$bal ")
-    async with ClientSession() as session:
-        response = await balaboba(str[1], intro=6, session=session)
-    await ctx.send(response)
+# аналогично надо воевать с дискордом (см. anekdot)
+# @bot.command()
+# async def film(ctx):
+#     page = requests.get(f"https://randomfilm.ru")
+#     page1 = page.content
+#     soup = BeautifulSoup(page1, "lxml")
+#     title1 = soup.find_all("h2")
+#     inf = soup.find_all("tr")
+#     st = str(inf).split('\n')
+#
+#     year = st[0]
+#     year = year[173:len(year)-5]
+#
+#     country = st[1]
+#     country = country[18:len(country)-5]
+#
+#     genre = st[2]
+#     genre = genre[16:len(genre)-5]
+#
+#     #print(st[3]) #yt]]недоделал продолжительность, режиссера и актера - они есть не увсех фильмов
+#     await ctx.send(f"{str(title1)[5:len(title1)-7]}\n\nГод: {year}\nСтрана: {country}\nЖанр: {genre}")
 
-@bot.command()
-async def film(ctx):
-    page = requests.get(f"https://randomfilm.ru")
-    page1 = page.content
-    soup = BeautifulSoup(page1, "lxml")
-    title1 = soup.find_all("h2")
-    inf = soup.find_all("tr")
-    st = str(inf).split('\n')
-
-    year = st[0]
-    year = year[173:len(year)-5]
-
-    country = st[1]
-    country = country[18:len(country)-5]
-
-    genre = st[2]
-    genre = genre[16:len(genre)-5]
-
-    #print(st[3]) #yt]]недоделал продолжительность, режиссера и актера - они есть не увсех фильмов
-    await ctx.send(f"{str(title1)[5:len(title1)-7]}\n\nГод: {year}\nСтрана: {country}\nЖанр: {genre}")
 
 @bot.command()
 async def who(ctx):
@@ -280,13 +177,13 @@ async def who(ctx):
     except:
         await ctx.send(f'{ctx.message.author.mention} В данный момент вы не находитесь в голосовом канале. Зайдите в тот канал, из которого вы хотите получить список участников.')
 
+
 @bot.command()
 async def help(message):
-    await message.send("Мои команды:\n$p - случайная пикча\n$roll - случайное число от 0 до 100\n"
-                       "$s - случаная цитата из аниме (c машина переводить)\n$play + ссылка - играет музыку с ютуба\n"
+    await message.send("Мои команды:\n$roll - случайное число от 0 до 100"
+                       "\n$play + ссылка - играет музыку с ютуба\n"
                        "$playl - играет музыку из моего локального плейлиста\n$skip - следующий трек\n"
-                       "$stop - отключает музыку\n$anekdot - отправляет анекдот\n"
-                       "$bal + сообщение - Балабоба от Яндекса\n"
-                       "$film - случайный фильм\n$who - участники голосового канала\n$leave - бот покидает голосовой канал")
+                       "$stop - отключает музыку (бот ливает)\n"
+                       "$who - участники голосового канала\n$leave - бот покидает голосовой канал")
 
 bot.run(settings['token'])
